04ac8997e9c69198ecf91c0e726ae4ebd3cd167b
Dinushika Hewage <dinushikahewage1993@gmail.com>
2026-04-04 19:42:56 +0800

    Implement DeletePlatform method in PlatformRepo
    Add method to delete a platform from the repository.

--- PlatformService/PlatformService/Data/PlatformRepo.cs
+++ PlatformService/PlatformService/Data/PlatformRepo.cs
@@ -31,6 +31,15 @@ namespace PlatformService.Data
             return _context.Platforms.FirstOrDefault(p => p.Id == id);
         }
 
+        public IEnumerable<Platform> DeletePlatform(Platform platform)
+        {
+            if (platform == null)
+            {
+                throw new ArgumentNullException(nameof(platform));
+            }
+            _context.Platforms.Remove(platform);
+        }
+
         public bool SaveChanges ()
         {
             return _context.SaveChanges() >= 0;
